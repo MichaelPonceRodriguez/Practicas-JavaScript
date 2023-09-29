e5c313ab0fc7b523a54bed4ccd5ae025ce365ed5
MichaelPonce <michaelponce1012@gmail.com>
2023-09-29 17:05:38 -0300

    Mejora Ejercicio Calendario de estaciones del año Argentina

--- Practica/1-1-ejercicio.js
+++ Practica/1-1-ejercicio.js
@@ -1,4 +1,4 @@
-//Ejercicio de Calendario de Estaciones del año Argentina
+//Ejercicio de Calendario de Estaciones del año Argentina (IF ELSE)
 /*
 Verano (21 de diciembre a 20 de marzo). 
 Otoño (21 de marzo a 20 de junio). 
@@ -39,4 +39,53 @@ else{
             else if(mes >= 10 && mes <= 11 && dia <= 31)console.log(estacion[3]);
             else if(mes == 12 && dia <= 20)console.log(estacion[3]);
         }
+}
+
+//--------------------------------------------------------------------------------------------------------------------------
+
+//Ejercicio de Calendario de Estaciones del año Argentina (SWITCH)
+/*
+Verano (21 de diciembre a 20 de marzo). 
+Otoño (21 de marzo a 20 de junio). 
+Invierno (21 de junio a 20 de septiembre). 
+Primavera (21 de septiembre a 20 de diciembre).
+*/ 
+mes = 6;
+dia = 20;
+
+if(dia > 31 || dia <=0){
+    console.log("Intente otro dia");
+}else{
+    switch(mes){
+        case 1:case 2:
+            console.log(estacion[0]);
+        break;
+        case 3:
+            if(dia <=20) console.log(estacion[0]);
+            else console.log(estacion[1]);
+        break;
+        case 4:case 5:
+            console.log(estacion[1]);
+        break;
+        case 6:
+            if(dia <=20) console.log(estacion[1]);
+            else console.log(estacion[2]);
+        break;
+        case 7:case 8:
+            console.log(estacion[2]);
+        break;
+        case 9:
+            if(dia <=20) console.log(estacion[2]);
+            else console.log(estacion[3]);
+        break;
+        case 10: case 11:
+            console.log(estacion[3]);
+        break;
+        case 12:
+            if(dia <=20) console.log(estacion[3]);
+            else console.log(estacion[0]);
+        break;
+        default:
+            console.log("Ingrese otro mes");
+    }
 }
