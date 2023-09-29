78fba604d731f6e965240c53adc1718b07d5f6e8
MichaelPonce <michaelponce1012@gmail.com>
2023-09-29 13:07:43 -0300

    primera practica JS

---
+++ Practica/01-leccion.js
@@ -0,0 +1,25 @@
+/* 
+Tipos de variables (String, int, boolean)
+Concatenacion de cadenas
+Impresion de mensaje por consola
+Sentencia de control if
+*/
+var nombre ="Michael";
+var apellido ="Ponce";
+
+console.log("Hola:" +nombre+" "+ apellido);
+console.log("Hola mundo desde JavaScript");
+
+
+var cinco= 5;
+var seis = 6;
+
+if(seis < cinco){
+    console.log("tiene sentido");
+}else{
+    console.log("no tiene sentido");
+}
+
+
+nombre += "ponce";
+console.log(nombre);
---
+++ Practica/02-leccion.js
@@ -0,0 +1,28 @@
+/*
+Operadores aritmeticos
+Sentencia de control Switch
+*/
+let numero = 21;
+let numero2 = 10;
+let resultado;
+// calculadora
+let opcion = 4;
+
+switch(opcion){
+    case 1:
+    resultado = numero + numero2;
+    console.log(resultado);
+        break;
+    case 2:
+        resultado = numero - numero2;
+        console.log(resultado);
+        break;
+    case 3:
+        resultado = numero * numero2;
+        console.log(resultado);
+        break;
+    case 4:
+        resultado = numero / numero2;
+        console.log(resultado);
+        break;        
+}
