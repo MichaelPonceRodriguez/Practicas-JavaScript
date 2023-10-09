5f9b49e7f12cb42e4dbfdef817865f759e870889
MichaelPonce <michaelponce1012@gmail.com>
2023-10-09 16:50:06 -0300

    ejercicio cuenta de banco

--- CuentaBancaria.js
+++ CuentaBancaria.js
@@ -26,17 +26,34 @@ class CuentaBancaria{
     }
 }
 
+class CajaDeAhorro extends CuentaBancaria{
+    
+    static _extraccionesPosibles = 10;
+    constructor(saldo){
+        super(saldo);
+    }
 
-
-
-
-
-
-
-
-
-
-let michael = new CuentaBancaria(100000);
-console.log(michael.saldo());
-console.log(michael.depositar(15000));
-console.log(michael.extraer(1000000));
+    extraccionesPosible(){
+        return this._extraccionesPosibles;
+    }
+    
+    extraccionesPosibles(monto){
+        return this._extraccionesPosibles <= monto;
+    }
+    hayExtraccionesPosibles(){
+        return this._extraccionesPosibles <= 10 && this._extraccionesPosibles > 0;
+    }
+    decrementarUnaExtraccion(){
+        --this._extraccionesPosibles;
+    }
+    realizarExtraccion(monto){
+        if(this.hayExtraccionesPosibles()){
+            this._saldo -= monto;
+            this.decrementarUnaExtraccion();
+        }else return false;
+    }
+    puedoExtraer(monto){
+        if(this.hayExtraccionesPosibles())return this._saldo > monto;
+        else return false;
+    }
+}
