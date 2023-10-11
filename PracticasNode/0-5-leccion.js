e7293a3dae1afc716b39fb3dc73e14ebfd2037f2
MichaelPonce <michaelponce1012@gmail.com>
2023-10-11 13:50:26 -0300

    Async Await

---
+++ PracticasNode/0-5-leccion.js
@@ -0,0 +1,38 @@
+//Async Await
+
+async function hola(nombre) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function() {
+            console.log("Hola, " + nombre);
+            resolve(nombre);
+        }, 1500);
+    
+    });
+}
+async function hablar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log("bla bla bla bla");
+            resolve(nombre);
+        }, 1000);
+    });
+}
+async function adios(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log("Adios", nombre);
+            resolve();
+        }, 1000);
+    });
+}
+
+async function main(){
+    let nombre = await hola('Michael');
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
+    console.log('Termino el proceso');
+}
+console.log('Iniciando el proceso...');
+main();
